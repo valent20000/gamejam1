0b839120762604956cb4bf312a0a8094a98f9b08
Lostindark20000 <valentin.maestracci@gmail.com>
2020-08-22 00:02:40 +0200

    Fixed player movement

--- GameJam1/Player.gd
+++ GameJam1/Player.gd
@@ -6,19 +6,16 @@ var velocity = Vector2()
 
 func get_input():
 	velocity = Vector2()
-	if Input.is_action_pressed('right'):
+	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
-	if Input.is_action_pressed('left'):
+	if Input.is_action_pressed('ui_left'):
 		velocity.x -= 1
-	if Input.is_action_pressed('down'):
+	if Input.is_action_pressed('ui_down'):
 		velocity.y += 1
-	if Input.is_action_pressed('up'):
+	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
-	position += velocity * delta
-	#position.x = clamp(position.x, 0, screen_size.x)
-	#position.y = clamp(position.y, 0, screen_size.y)
