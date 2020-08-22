8bff090d94927fb6997803233a0e484f0d8f70ca
valent20000 <valentin.maestracci@gmail.com>
2020-08-22 15:10:45 +0200

    Can't fire bullets at will anymore

--- squaretaker/Bullet.gd
+++ squaretaker/Bullet.gd
@@ -2,8 +2,12 @@ extends KinematicBody2D
 
 var velocity = Vector2(0,1)
 export var speed = 20
+export var life = 3
 
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * speed * delta)
 	if collision_info:
 		velocity = velocity.bounce(collision_info.normal)
+		life-=1
+		if life < 0:
+			queue_free()
--- squaretaker/Gun.gd
+++ squaretaker/Gun.gd
@@ -2,20 +2,40 @@ extends Node2D
 
 var mousePos
 var size
-var vec
 
 export var Bullet = preload("res://Bullet.tscn")
+export var cooldown = 5
+export var ammo = 6
+export var time_between_shoot = 0.5
+var ammo_left = ammo
+var no_ammo = false
+var can_shoot = true
 
 signal shoot(bullet, rotation, position)
 
 func _ready():
 	size = $GunSprite.frames.get_frame("default", 0).get_size()
-	vec = Vector2(0, size.y)
 
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
 	rotate(PI/2)
 
 func _on_fire():
-	var vec = Vector2(0, size.y).rotated(get_global_rotation())
-	emit_signal("shoot", Bullet, get_global_rotation(), get_global_position() - vec)
+	if can_shoot:
+		$TimeBetweenShoot.start()
+		can_shoot = false
+		if ammo_left:
+			var vec = Vector2(0, size.y).rotated(get_global_rotation())
+			emit_signal("shoot", Bullet, get_global_rotation(), get_global_position() - vec)
+			ammo_left-=1
+			if ammo_left <= 0:
+				no_ammo = true
+				$Reload.start()
+
+
+func _on_TimeBetweenShoot_timeout():
+	can_shoot = true
+
+func _on_Reload_timeout():
+	no_ammo = false
+	ammo_left = ammo
