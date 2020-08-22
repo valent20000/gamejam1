1ea1442a27ff11a1bcec89d38dec99a1f6a78a14
valent20000 <valentin.maestracci@gmail.com>
2020-08-22 11:49:19 +0200

    Delete q

--- GameJam1/Levels/LevelTemplate/BulletSystem.gd
+++ GameJam1/Levels/LevelTemplate/BulletSystem.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
-func _on_shoot(bullet, rotation, position):
-	pass # Replace with function body.
-
-
-func _on_Player_shoot(bullet, direction, location):
+func _on_shoot(bullet, direction, location):
 	var b = bullet.instance()
 	add_child(b)
 	b.rotation = direction
