ac1dcac3c251230fab446d4828d1adfd2b08158d
valent20000 <valentin.maestracci@gmail.com>
2020-08-22 14:44:34 +0200

    Bullets are working mother...

--- squaretaker/Bullet.gd
+++ squaretaker/Bullet.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
-var velocity = Vector2(0,0)
+var velocity = Vector2(0,1)
+export var speed = 20
 
 func _physics_process(delta):
-	var collision_info = move_and_collide(velocity * delta)
+	var collision_info = move_and_collide(velocity * speed * delta)
 	if collision_info:
 		velocity = velocity.bounce(collision_info.normal)
