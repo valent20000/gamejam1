4c64ae756373cc7382624aadaaa2cf8e8c2b2d1d
valent20000 <valentin.maestracci@gmail.com>
2020-08-24 19:27:49 +0200

    Game now working

--- squaretaker/Play.gd
+++ squaretaker/Play.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
-var next_scene = preload("res://Levels/Tutorial2.tscn")
+var next_scene = preload("res://Levels/Tutorial.tscn")
 
 func _on_Play_pressed():
 	get_tree().change_scene_to(next_scene)
