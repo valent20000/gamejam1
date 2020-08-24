1312b110a350d68a268033295964e0ebb10332bf
Gariwald <Vassili.Maillet@grenoble-inp.org>
2020-08-24 15:22:22 +0200

    changed shield appearance and size

--- squaretaker/Shield/Shield.gd
+++ squaretaker/Shield/Shield.gd
@@ -4,19 +4,19 @@ var available = true
 
 func _ready():
 	$CollisionShape2D.disabled = true
-	$Sprite.hide()
+	$AnimatedSprite.hide()
 
 func _process(delta):
 	if Input.is_action_pressed("right_click") && available:
 		$CollisionShape2D.disabled = false
-		$Sprite.show()
+		$AnimatedSprite.show()
 		$ShieldDuration.start()
 		$ShieldCooldown.start()
 		available = false
 
 func _on_ShieldDuration_timeout() -> void:
 	$CollisionShape2D.disabled = true
-	$Sprite.hide()
+	$AnimatedSprite.hide()
 	available = false
 
 func _on_ShieldCooldown_timeout() -> void:
